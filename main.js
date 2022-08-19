5ca3c8e46aae0425a7c31a552f482b82f80dc2f6
GabrielOJorge <goliver331@protonmail.com>
2022-08-19 15:52:52 -0300

    add 's' to http links

--- main.js
+++ main.js
@@ -207,7 +207,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 const getLocations = async location => {
-  const geoCode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=c0ce5c434457e791f019f4b386f41af3`, { mode: 'cors' })
+  const geoCode = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=c0ce5c434457e791f019f4b386f41af3`, { mode: 'cors' })
   const geoCodeData = await geoCode.json();
 
   return geoCodeData;
